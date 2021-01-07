9a1c21e411868207b2a03049e289081530a5d16f
ganzes <charczuk.arkadiusz@gmail.com>
2021-01-07 23:00:21 +0100

    first commit

---
+++ src/test/java/booksontheshelfbackend/BooksOnTheShelfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package booksontheshelfbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksOnTheShelfBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
